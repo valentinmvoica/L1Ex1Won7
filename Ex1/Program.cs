d8c442653c9461a4492ad46bb17342cdf6c7c8cb
v <v@DESKTOP-DR0IDJ0>
2024-02-14 20:49:58 +0200

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("introduceti x");
+int x = int.Parse(Console.ReadLine());
+
+
+if (x > 0)
+{
+    Console.WriteLine("numarul e pozitiv " + x);
+    int y = 3;
+
+    if (y > 5)
+    {
+    }
+    else
+    {
+
+        Console.WriteLine(y + " mai mare ca 5");
+    }
+}
+else
+{
+    Console.WriteLine("<0");
+}
+
+
+
+//Console.WriteLine("introduceti latimea");
+//int latime =  int.Parse(Console.ReadLine());
+
+
+
+//Console.WriteLine("introduceti inaltimea");
+//int inaltime = int.Parse(Console.ReadLine());
+
+
+//Console.WriteLine("introduceti adancimea");
+//int adancime = int.Parse(Console.ReadLine());
+
+
+//int volumulParalelipipeduluiDreptunghic = latime * inaltime * adancime;
+
+//Console.WriteLine("Volumul paralelipipedului");
+//Console.WriteLine(volumulParalelipipeduluiDreptunghic);
